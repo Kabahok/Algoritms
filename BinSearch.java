396d4d66866dde02b0bc33924049212bd993c963
egor avdeev <workegoravd@gmail.com>
2023-05-17 21:52:52 +0300

    Recursion algoritm

--- BinSearch.java
+++ BinSearch.java
@@ -2,10 +2,13 @@ public class BinSearch {
     public static void main(String[] args) {
         int[] prices = { 13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30 };
 
-        System.out.println("Для 31: " + countMore(prices, 31)); // 0
-        System.out.println("Для 26: " + countMore(prices, 26)); // 2
-        System.out.println("Для 25: " + countMore(prices, 25)); // 2
-        System.out.println("Для 20: " + countMore(prices, 20)); // 8
+//        На вход подается упорядочный массив с ценами на товары и цена сертификата.
+//        Необходимо найти количество товаров, которые нельзя купить с
+//        помощью сертификата
+
+            String str1 = new String("A");
+            String str2 = new String("A");
+        System.out.println(str1 == str2);
 
     }
 
@@ -23,14 +26,31 @@ public class BinSearch {
 
         while (left < right) {
             int middle = (left + right) / 2;
-            if ((prices[middle] >= money && prices[middle-1] < money) || (prices[middle] > money && prices[middle-1] == money)) {
+
+            if (prices[middle] == money && prices[middle+1] != money ) {
                 return prices.length - middle;
-            } else if ((prices[middle] < money) || (prices[middle] == money && prices[middle+1] == money) || (prices[middle] == money && prices[middle+1] > money)) {
-                left = middle + 1;
+            } else if (prices[middle] < money) {
+                if (prices[middle + 1] < money) {
+                    left = middle + 1;
+                } else if (prices[middle+1] > money) {
+                    return prices.length - middle -1;
+                }
             } else if (prices[middle] > money && prices[middle-1] > money) {
-                right = middle - 1;
+                right = middle -1;
             }
         }
+
+//        while (left < right) {
+//            int middle = (left + right) / 2;
+//            if ((prices[middle] >= money && prices[middle-1] < money) || (prices[middle] > money && prices[middle-1] == money)) {
+//                return prices.length - middle;
+//            } else if ((prices[middle] < money) || (prices[middle] == money && prices[middle+1] == money) || (prices[middle] == money && prices[middle+1] > money)) {
+//                left = middle + 1;
+//            } else if (prices[middle] > money && prices[middle-1] > money) {
+//                right = middle - 1;
+//            }
+//        }
+
 
         return 0;
     }
